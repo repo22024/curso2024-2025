931eb798660f96137ec74dc696bb9b57c9775a0c
Abián <70582564+abian22@users.noreply.github.com>
2025-03-03 17:56:35 +0000

    datos de pruebas insertados en insercion.sql y añadidos en el informe

---
+++ BASES DE DATOS  1º DAM/PROYECTOS DE BASES DE DATOS/abian/scripts/insercion.sql
@@ -0,0 +1,81 @@
+USE carrentalx; 
+
+-- -----------------------------------------------------
+-- Datos clientes
+-- -----------------------------------------------------
+
+INSERT INTO clientes 
+
+VALUES   
+
+(DEFAULT, 'Juan', 'Pérez', '12345678A', 'juan.perez@gmail.com', 612345678, 'A'),  
+
+(DEFAULT, 'Ana', 'García', '87654321B', 'ana.garcia@gmail.com', 623456789, 'B'),  
+
+(DEFAULT, 'Carlos', 'López', '11223344C', 'carlos.lopez@gmail.com', 634567890, 'C'), 
+
+(DEFAULT, 'Manuel', 'Jiménez', '3215678A', 'manuel.jimenez@gmail.com', 652145423, 'A,B');
+
+-- -----------------------------------------------------
+-- Datos sucursal
+-- -----------------------------------------------------
+
+INSERT INTO sucursal 
+
+VALUES  
+
+(DEFAULT, 'Sucursal Central', 'Calle Mayor 123', 912345678, 'central@gmail.com'), 
+
+(DEFAULT, 'Sucursal Norte', 'Calle Norte 456', 923456789, 'norte@gmail.com'), 
+
+(DEFAULT, 'Sucursal Sur', 'Calle Sur 789', 934567890, 'sur@gmail.com'); 
+
+-- -----------------------------------------------------
+-- Datos vehículos
+-- -----------------------------------------------------
+
+INSERT INTO vehiculos 
+
+VALUES  
+
+(DEFAULT, '1234ABC', 'Ford Fiesta', 2020, 'Rojo', 'gasolina', 100.5, 'SI', 1, 1), 
+
+(DEFAULT, '5678DEF', 'BMW X5', 2019, 'Azul', 'diesel', 250.75, 'NO', 2, 2), 
+
+(DEFAULT, '9101GHI', 'Tesla Model 3', 2021, 'Negro', 'electrico', 500.0, 'SI', 3, 3), 
+
+(DEFAULT, '123ABC', 'Toyota Corolla', 2020, 'Rojo', 'gasolina', 35.99, 'SI', 1,1), 
+
+(DEFAULT, '456DEF', 'Ford Focus', 2021, 'Azul', 'gasolina,diesel', 40.50, 'SI', 2,2);
+
+-- -----------------------------------------------------
+-- Datos reservas
+-- -----------------------------------------------------
+
+INSERT INTO reservas 
+
+VALUES  
+
+(DEFAULT, 150.0, '2025-03-10 10:00:00', '2025-03-12 10:00:00', 'pendiente', 1, 1, 1), 
+
+(DEFAULT, 200.5, '2025-03-15 12:00:00', '2025-03-18 12:00:00', 'enCurso', 2, 2, 2), 
+
+(DEFAULT, 350.0, '2025-03-20 09:00:00', '2025-03-22 09:00:00', 'finalizado', 3, 3, 3), 
+
+(DEFAULT, 350.0, '2025-03-24 19:00:00', '2025-03-28 09:00:00', 'anulado', 3, 3, 3);
+
+-- -----------------------------------------------------
+-- Datos empleados
+-- ----------------------------------------------------
+
+INSERT INTO empleados 
+
+VALUES  
+
+(DEFAULT, 'Gerente', 'María', 'Martínez', 'maria.martinez@gmail.com', 611223344, 1), 
+
+(DEFAULT, 'Asesor', 'Pedro', 'Gómez', 'pedro.gomez@gmail.com', 622334455, 2), 
+
+(DEFAULT, 'Recepcionista', 'Lucía', 'Sánchez', 'lucia.sanchez@gmail.com', 633445566, 3) 
+
+ 
